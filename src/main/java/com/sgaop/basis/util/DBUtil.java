27dac71272969059eb0b9878d4247bfaecf010f7
Huc <306955302@qq.com>
2016-12-16 17:01:30 +0800

    增加国际化功能，dao优化

--- src/main/java/com/sgaop/basis/util/DBUtil.java
+++ src/main/java/com/sgaop/basis/util/DBUtil.java
@@ -208,6 +208,9 @@ public class DBUtil {
     }
 
     public static <E> E RecordToEntity(Class cls, TableInfo tableInfo, Record record) throws Exception {
+        if(record.size()==0){
+            return null;
+        }
         Object obj = cls.newInstance();
         for (String colum : tableInfo.getColums()) {
             TableFiled tableFiled = tableInfo.getDaoFiled(colum);
